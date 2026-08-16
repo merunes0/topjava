0cf0b60b3ff68ce943f67566630f409223ce5493
sidebyside <merunes0@yandex.rui>
2026-08-16 19:35:56 +0300

    Implement filteredByCycles and filteredByStreams

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,13 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -24,16 +26,66 @@ public class UserMealsUtil {
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+
+
+//        System.out.println(getAllDescriptions(meals));
+//        System.out.println(countMealsWithCaloriesAbove(meals,500 ));
+//        System.out.println(findMostCaloricMeal(meals));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+
+        HashMap<LocalDate, Integer> caloriesByDate = new HashMap<>();
+        List<UserMealWithExcess> meals1 = new ArrayList<>();
+
+        for (UserMeal meal : meals) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            caloriesByDate.put(date, caloriesByDate.getOrDefault(date, 0) + meal.getCalories());
+        }
+        for (UserMeal meal : meals)
+            if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
+                boolean excess = false;
+                LocalDate date = meal.getDateTime().toLocalDate();
+                if (caloriesByDate.get(date) > caloriesPerDay) {
+                    excess = true;
+                }
+                meals1.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess));
+            }
+
+        return meals1;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO Implement by streams
-        return null;
+
+        Map<LocalDate, Integer> caloriesByDate = meals.stream()
+                .collect(Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(),
+                        Collectors.summingInt(meal -> meal.getCalories())));
+
+        return meals.stream()
+                .filter(meal -> TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> {
+                    boolean excess = caloriesByDate.get(meal.getDateTime().toLocalDate()) > caloriesPerDay;
+                    return new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
+                })
+                .collect(Collectors.toList());
     }
+
+
+//    public static List<String> getAllDescriptions(List<UserMeal> meals) {
+//        return meals.stream()
+//                .map(UserMeal::getDescription)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public static long countMealsWithCaloriesAbove(List<UserMeal> meals, int caloriesThreshold){
+//        return meals.stream()
+//                .filter(meal -> meal.getCalories() > caloriesThreshold).count();
+//    }
+//    public static Optional<UserMeal> findMostCaloricMeal(List<UserMeal> meals){
+//        return meals.stream()
+//                .max(Comparator.comparingInt(UserMeal::getCalories));
+//    }
 }
